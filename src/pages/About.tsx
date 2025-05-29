ef548b2007d3b23dd50edad0e8cb67807053f9ae
christopherzupanc <christopherzupanc@outlook.com>
2025-05-29 18:47:34 +0200

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -30,7 +30,7 @@ export function About() {
                               z-20"></div>
               </div>
               <div className="text-center space-y-4">
-                <h2 className="text-2xl font-bold text-white">Thomas Zupanc</h2>
+                <h2 className="text-2xl font-bold text-white">Christopher Zupanc</h2>
                 <p className="text-gray-300 text-lg">
                   Maschinenbauingenieur & Technischer Entwickler
                 </p>
